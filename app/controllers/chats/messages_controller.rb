99876580971ec3507a84c79d462cd635f417b659
alpbourne <alpbourne@gmail.com>
2018-04-29 23:04:42 -0400

    added code to messages controller, sending and receiving messages not working, added send sms job

--- app/controllers/chats/messages_controller.rb
+++ app/controllers/chats/messages_controller.rb
@@ -1,6 +1,7 @@
 class Chats::MessagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:reply]
   before_action :set_chat, except: [:repy]
+  skip_before_action :verify_authenticity_token, only: [:reply]
 
   def index
     @messages = @chat.messages.order("create_at ASC")
@@ -11,10 +12,23 @@ class Chats::MessagesController < ApplicationController
     @message = @chat.messages.build(message_params)
     @message.update(to: @chat.friend.phone, status: 'pending', direction: 'outbound-api')
     if @message.save
+      SendSmsJob.perform_later(@message)
       redirect_to chat_messages_path(@chat), notice: 'Message sent'
     else
       redirect_to chat_messages_path(@chat), alert: 'Message failed'
     end
+  end
+
+  def reply
+    from = params[:From].gsub(/^\+\d/, '')
+    body = params[:Body]
+    status = params[:SmsStatus]
+    direction = 'inbound'
+    message_sid = params[:MessageSid]
+    friend = Friend.where("phone like ?", "%#{from}%").first
+    @chat = Chat.where(friend: friend).first
+    @chat.messages.build(body: body, direction: direction, status: status, from: from, message_sid: message_sid).save
+    head :ok, content_type: "text/html"
   end
 
   private
